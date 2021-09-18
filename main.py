7fd828c6987569078c212a95fec78ff54ec9d71d
Anas <ansmr002@gmail.com>
2021-09-19 02:43:25 +0300

    oop

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
-import sys
-
-
-class Student:
-    counter = 0
-    school = 'alnjar'
+class Person:
+    school = 'al-njar'
     laptop = ''
 
-    def __init__(self, name, age, gender):
+    def __init__(self, name, age, gender, pid):
         self.name = name
         self.age = age
         self.gender = gender
-
-    def getNme(self):
-        return self.name
+        self.pid = pid
 
     def setName(self, name):
         self.name = name
+
+    def getName(self):
+        return self.name
 
     def setAge(self, age):
         self.age = age
@@ -29,6 +26,37 @@ class Student:
     def getGender(self):
         return self.gender
 
+    def setPid(self, pid):
+        self.pid = pid
+
+    def getPid(self):
+        return self.pid
+
+    def info(self):
+        print(
+            f"the person name is {self.getName()}, the age is {self.getAge()}, the gender is {self.getGender()}, the person id is {self.getPid()}",
+            end=' ** ')
+
+
+class Student(Person):
+
+    def __init__(self, name, age, gender, pid, course, grade):
+        super().__init__(name, age, gender, pid)
+        self.course = course
+        self.grade = grade
+
+    def setCourse(self, course):
+        self.course = course
+
+    def getCourse(self):
+        return self.course
+
+    def setGrade(self, grade):
+        self.grade = grade
+
+    def getGrade(self):
+        return self.grade
+
     def setLaptop(self, laptop):
         self.laptop = laptop
         self.laptop.student = self
@@ -36,26 +64,21 @@ class Student:
     def getLaptop(self):
         return self.laptop
 
-    def info(self):
-        print(
-            f'welcome {self.name} your gender is {self.gender}, age is {self.age}, your school is {self.school} ')
-
     def checkLaptop(self):
-        if self.laptop == '':
-            return False
-        return True
+        if self.laptop != '':
+            return True
+        return False
 
-    @classmethod
-    def setCounter(cls):
-        cls.counter += 1
-
-    @classmethod
-    def getCounter(cls):
-        return cls.counter
-
-    @staticmethod
-    def printInfo():
-        print('this is student class.................')
+    def info(self):
+        super(Student, self).info()
+        print(f'your course {self.course}, your grade is {self.grade}, your '
+              f'school is {self.school}')
+        if self.laptop != '':
+            self.laptop.info()
+            print(f'this laptop belong to {self.getLaptop().getStudent().getName()}')
+        else:
+            print('this student dont have any laptop!!!')
+        print('*************************************************************************************')
 
     class Laptop:
         student = ''
@@ -65,54 +88,87 @@ class Student:
             self.cpu = cpu
             self.ram = ram
 
-        def setStudent(self, student):
+        def setBrand(self, brand):
+            self.brand = brand
+
+        def getBrand(self):
+            return self.brand
+
+        def setCpu(self, cpu):
+            self.cpu = cpu
+
+        def getCpu(self):
+            return self.cpu
+
+        def setRam(self, ram):
+            self.ram = ram
+
+        def getRam(self):
+            return self.ram
+
+        def checkLaptop(self):
+            if self.student != '':
+                return True
+            return False
+
+        def setStudent(self,student):
             self.student = student
             self.student.laptop = self
 
         def getStudent(self):
             return self.student
 
-        def checkStudent(self):
-            if self.student == '':
-                return False
-            return True
-
         def info(self):
-            print(
-                f'the brand is {self.brand} and cpu is {self.cpu}, ram is {self.ram}')
+            print(f'the laptop brand is {self.brand}, the cpu is {self.cpu}, the ram is {self.ram}')
 
 
-class Course:
-    pass
+class Master(Person):
+    salary_type = '$'
+
+    def __init__(self, name, age, gender, pid, salary, course):
+        super().__init__(name, age, gender, pid)
+        self.salary = salary
+        self.course = course
+
+    def setSalary(self, salary):
+        self.salary = salary
+
+    def getSalary(self):
+        return self.salary
+
+    def setCourse(self, course):
+        self.course = course
+
+    def getCourse(self):
+        return self.course
+
+    def setSalaryType(self, salary_type):
+        self.salary_type = salary_type
+
+    def getSalaryType(self):
+        return self.salary_type
+
+    @classmethod
+    def changeSalaryType(cls, salary_type):
+        cls.salary_type = salary_type
+
+    def info(self):
+        super(Master, self).info()
+        print(f'the salary is {self.getSalary()}{self.salary_type}, the courses is {self.getCourse()}')
 
 
-student = Student('anas', 23, 'male')
+st = Student('anas', 20, 'male', 23221, 'python', 89)
 laptop = Student.Laptop('msi', 'cor i7', 16)
+st.setLaptop(laptop)
+st.info()
 
-laptop.info()
 
+st = Student('wesam', 23, 'female', 23221, 'c++', 89)
+laptop = Student.Laptop('dell', 'cor i5', 8)
+st.setLaptop(laptop)
+st.info()
 
-students = []
-
-num = int(input('enter the number of student: '))
-
-for i in range(num):
-    name = input(f'enter the name of student {i + 1}) ')
-    age = int(input(f'enter the age of student {i + 1}) '))
-    gender = input(f'enter the gender of student {i + 1}) ')
-    student = Student(name, age, gender)
-    Student.setCounter()
-    ask = input('###### do you want add laptop from this student? answer y or y ######: ')
-    if ask.lower() == 'yes' or ask.lower() == 'y':
-        brand = input('enter the brand: ')
-        cpu = input('enter the type of cpu: ')
-        ram = int(input('enter the number of ram: '))
-        student.setLaptop(Student.Laptop(brand, cpu, ram))
-    students.append(student)
-    print('##########################################################################################')
-
-for student in students:
-    student.info()
-    if student.checkLaptop():
-        student.getLaptop().info()
-        print('this laptop belong to ', student.getNme())
+st = Student('eman', 40, 'female', 23221, 'c++', 89)
+laptop = Student.Laptop('apple', 'cor i3', 4)
+laptop.setStudent(st)
+st.info()
